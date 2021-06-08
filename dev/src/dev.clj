65666f09bbb0e237ede5a5609a6c7c65a9036503
Felix <zen9.felix@gmail.com>
2021-06-08 15:36:33 +0800

    Make refresh dirs explicit

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -1,6 +1,9 @@
 (ns dev
   (:require [stanne.system :as system]
+            [clojure.tools.namespace.repl]
             [integrant.repl :refer [go reset]]))
+
+(clojure.tools.namespace.repl/set-refresh-dirs "dev" "src")
 
 (integrant.repl/set-prep! (constantly (system/config :dev)))
 
