21d68c012268e0bf7b955dcbb037e5b7c0fe06c3
Felix <zen9.felix@gmail.com>
2021-06-09 17:03:07 +0800

    Parse bank list

--- src/stanne/fpx/be.clj
+++ src/stanne/fpx/be.clj
@@ -2,27 +2,54 @@
   (:require
    [clojure.string :as str]
    [org.httpkit.client :as http]
-   [stanne.fpx.common :refer [config]]
+   [stanne.fpx.common :as common]
    [stanne.fpx.utils :as utils]))
 
 (defn- bank-list-request [{:keys [exchange-id msg-token fpx-version pki endpoints]}]
   (let [msg-type "BE"
         url (:bank-list endpoints)
-        validation (str/join "|" [msg-token msg-type exchange-id fpx-version])
-        checksum (-> validation
-                     (utils/sign {:private-key (:merchant-key pki)}))
+        request-params (str/join "|" [msg-token msg-type exchange-id fpx-version])
+        signature (-> request-params
+                      (utils/sign {:private-key (:merchant-key pki)}))
         form-params {:fpx_msgType msg-type
                      :fpx_msgToken msg-token
                      :fpx_sellerExId exchange-id
                      :fpx_version fpx-version
-                     :fpx_checkSum checksum}]
+                     :fpx_checkSum signature}]
     {:url url
-     :form-params form-params
-     :validation validation}))
+     :form-params form-params}))
+
+(defn bank-list [config banks]
+  (let [{:keys [url form-params]} (bank-list-request config)
+        resp @(http/post url {:form-params form-params
+                              :as :text})
+        info (-> resp :body utils/parse-nvp)
+        response-params (->> info
+                             ((apply juxt [:fpx_bankList
+                                           :fpx_msgToken
+                                           :fpx_msgType
+                                           :fpx_sellerExId]))
+                             (str/join "|"))
+        signature (:fpx_checkSum info)
+        public-key (-> config :pki :fpx-cert)
+        checksum-ok? (utils/verify response-params signature {:public-key public-key})]
+    (when-not checksum-ok?
+      (throw (ex-info "Invalid checksum"
+                      {:api "BE"
+                       :response-params response-params
+                       :signature signature
+                       :public-key public-key})))
+    (let [process (comp (fn [[a b]] [a {:code a
+                                        :name (banks a)
+                                        :status b}])
+                        #(str/split % #"~"))]
+      (-> info
+          :fpx_bankList
+          (str/split #",")
+          ((partial map process))
+          ((partial into {}))))))
 
 (comment
   ;; bank list
-  (let [{:keys [url form-params]} (bank-list-request (config :dev))
-        resp @(http/post url {:form-params form-params
-                              :as :text})]
-    (:body resp)))
+  (bank-list (common/config :dev)
+             (common/bank-mapping :dev)))
