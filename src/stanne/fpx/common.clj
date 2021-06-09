d7422f0ec8904c14a432870a2af6b1fca5789f19
Felix <zen9.felix@gmail.com>
2021-06-09 21:21:20 +0800

    Add tnc to config

--- src/stanne/fpx/common.clj
+++ src/stanne/fpx/common.clj
@@ -7,20 +7,20 @@
           :msg-token "01" ;; B2C
           :fpx-version "7.0"}
          (case env
-           :prod
-           {:env :prod
-            :pki {:merchant-key "/etc/fpx/EX00011982.key"
-                  :fpx-cert "/etc/fpx/fpxprod_Merchant.cer"}
-            :endpoints {:auth-request "https://www.mepsfpx.com.my/FPXMain/seller2DReceiver.jsp"
-                        :auth-cancel "https://www.mepsfpx.com.my/FPXMain/FPXMain/sellerReqCancel.jsp"
-                        :bank-list "https://www.mepsfpx.com.my/FPXMain/RetrieveBankList"}}
-           :dev
-           {:env :dev
-            :pki {:merchant-key "/etc/fpx/EX00011982.key"
-                  :fpx-cert "/etc/fpx/fpxuat.cer"}
-            :endpoints {:auth-request "https://uat.mepsfpx.com.my/FPXMain/seller2DReceiver.jsp"
-                        :auth-cancel "https://uat.mepsfpx.com.my/FPXMain/FPXMain/sellerReqCancel.jsp"
-                        :bank-list "https://uat.mepsfpx.com.my/FPXMain/RetrieveBankList"}})))
+           :prod {:env :prod
+                  :pki {:merchant-key "/etc/fpx/EX00011982.key"
+                        :fpx-cert "/etc/fpx/fpxprod_Merchant.cer"}
+                  :endpoints {:tnc "https://uat.mepsfpx.com.my/FPXMain/termsAndConditions.jsp"
+                              :auth-request "https://www.mepsfpx.com.my/FPXMain/seller2DReceiver.jsp"
+                              :auth-cancel "https://www.mepsfpx.com.my/FPXMain/FPXMain/sellerReqCancel.jsp"
+                              :bank-list "https://www.mepsfpx.com.my/FPXMain/RetrieveBankList"}}
+           :dev {:env :dev
+                 :pki {:merchant-key "/etc/fpx/EX00011982.key"
+                       :fpx-cert "/etc/fpx/fpxuat.cer"}
+                 :endpoints {:tnc "https://www.mepsfpx.com.my/FPXMain/termsAndConditions.jsp"
+                             :auth-request "https://uat.mepsfpx.com.my/FPXMain/seller2DReceiver.jsp"
+                             :auth-cancel "https://uat.mepsfpx.com.my/FPXMain/FPXMain/sellerReqCancel.jsp"
+                             :bank-list "https://uat.mepsfpx.com.my/FPXMain/RetrieveBankList"}})))
 
 (defn bank-mapping [env]
   (walk/stringify-keys
