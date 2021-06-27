57aa79767cc8f46b6eb76ab29513a2313bb5ce4d
Felix <zen9.felix@gmail.com>
2021-06-27 15:04:40 +0800

    cipher ok

--- src/stanne/cybersource/core.clj
+++ src/stanne/cybersource/core.clj
@@ -1,9 +1,28 @@
 (ns stanne.cybersource.core
   (:require [environ.core :refer [env]]
             [clojure.spec.alpha :as s]
-            [stanne.cybersource.utils :refer [txn-uuid current-date-time parse-num-str]]))
+            [stanne.cybersource.utils :refer [txn-uuid current-date-time parse-num-str to-nvp mk-hmac-hash]]
+            [clojure.string :as str]))
 
-;;; SPEC ;;;
+(def config
+  {:endpoint (env :cybersource-post-endpoint)
+   :profile-id (env :cybersource-profile-id)
+   :access-key (env :cybersource-access-key)
+   :secret (env :cybersource-secret)
+   :currency "MYR"})
+
+(def ^:private required-fields
+  [::access_key
+   ::profile_id
+   ::transaction_type
+   ::transaction_uuid
+   ::reference_number
+   ::amount
+   ::currency
+   ::locale
+   ::signed_field_names
+   ::unsigned_field_names
+   ::signed_date_time])
 
 (s/def ::currency #{"MYR"})
 (s/def ::locale #{"en"})
@@ -12,44 +31,24 @@
 (s/def ::signed_date_time string?) ;; 2021-06-27T02:17:41Z
 (s/def ::transaction_uuid uuid?)
 (s/def ::required-signed-fields
-  (s/keys :req-un
-          [::access_key
-           ::profile_id
-           ::transaction_type
-           ::transaction_uuid
-           ::reference_number
-           ::amount
-           ::currency
-           ::locale
-           ::signed_field_names
-           ::unsigned_field_names
-           ::signed_date_time]))
-
-;;; Main ;;;
-
-(defn- config []
-  {:endpoint (env :cybersource-post-endpoint)
-   :profile-id (env :cybersource-profile-id)
-   :access-key (env :cybersource-access-key)
-   :secret (env :cybersource-secret)
-   :currency "MYR"})
+  (s/keys :req (vec required-fields)))
 
 (defn- default-params [{:keys [access-key profile-id currency]}]
-  {:access_key access-key
-   :profile_id profile-id
-   :transaction_type "authorization"
-   :transaction_uuid (txn-uuid)
-   :currency currency
-   :locale "en"
-   :signed_date_time (current-date-time)
-   :signed_field_names "access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency"
-   :unsigned_field_names ""})
+  {::access_key access-key
+   ::profile_id profile-id
+   ::transaction_type "authorization"
+   ::transaction_uuid (txn-uuid)
+   ::currency currency
+   ::locale "en"
+   ::signed_date_time (current-date-time)
+   ::signed_field_names (->> (map name required-fields) (str/join ","))
+   ::unsigned_field_names ""})
 
 (defn mk-params [reference-number amount]
-  (let [params (-> (config)
+  (let [params (-> config
                    default-params
-                   (merge {:reference_number reference-number
-                           :amount (parse-num-str amount)}))
+                   (merge {::reference_number reference-number
+                           ::amount (parse-num-str amount)}))
         parsed (s/conform ::required-signed-fields params)]
 
     (if (s/invalid? parsed)
@@ -57,4 +56,6 @@
       parsed)))
 
 (comment
-  (mk-params "ref1" "-12"))
+  (let [params (mk-params "ref1" "12.10")]
+    (-> (to-nvp params)
+        (mk-hmac-hash config))))
--- src/stanne/cybersource/utils.clj
+++ src/stanne/cybersource/utils.clj
@@ -1,5 +1,8 @@
 (ns stanne.cybersource.utils
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [buddy.core.mac :as mac]
+            [clojure.string :as str]
+            [buddy.core.codecs :as codecs]))
 
 (defn txn-uuid []
   (java.util.UUID/randomUUID))
@@ -13,6 +16,18 @@
 
 (defn parse-num-str [amount]
   (edn/read-string amount))
+
+(defn to-nvp [params]
+  (->> [(name n) v]
+       (str/join "=")
+       (for [[n v] params])
+       (str/join ",")))
+
+(defn mk-hmac-hash [data {:keys [secret]}]
+  (-> (mac/hash data {:key secret
+                      :alg :hmac+sha256})
+      (codecs/bytes->b64)
+      (codecs/bytes->str)))
 
 (comment
   (current-date-time)
